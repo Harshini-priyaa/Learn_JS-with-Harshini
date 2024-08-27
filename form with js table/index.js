e483024ddc9e6540a7bda6b6df1f91278e57e8ac
Harshini-priyaa <harshinipriyaa1112@gmail.com>
2024-08-27 22:47:05 +0530

    Form using JS

---
+++ form with js table/index.js
@@ -0,0 +1,9 @@
+const tbody = document.createElement('tbody')
+const tr = document.createElement('tr')
+const th = document.createElement('th')
+const td = document.createElement('td')
+
+th.textContent = 2
+
+td.textContent = "Harshini"
+td.textContent = "  "
