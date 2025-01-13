b80faa3457ef7703dd3097f0dc2fd0e8ee960bba
Harshini-priyaa <harshinipriyaa1112@gmail.com>
2025-01-13 22:35:40 +0530

    Day2

---
+++ Day2.js
@@ -0,0 +1,10 @@
+// Variables are Containers for Storing Data
+// JavaScript Variables can be declared in 4 ways:
+
+// Automatically
+// Using var
+// Using let
+// Using const
+
+let a = 10;
+console.log(a);
